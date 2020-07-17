41fdf997c7327ffa81c41b525131be5cc532c3b0
radkin <noeldamonmiller@gmail.com>
2020-07-17 07:59:21 -0700

    search_customers route set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//custom
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'searchCustomers', component: SearchCustomersComponent },
+  { path: '', redirectTo: '/searchCustomers', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCustomersComponent
   ],
   imports: [
     BrowserModule,
